4c9729f363f74ddeedf19e5fe2435413a8fcc69e
Shearry Malone <sleam32@gmail.com>
2013-05-14 22:17:44 -0500

    initial commit

--- models/comicbook.rb
+++ models/comicbook.rb
@@ -1,4 +1,5 @@
 require 'active_record'
 
 class ComicBook < ActiveRecord::Base
+	# create a table called ComicBooks
 end
--- test/test_adding_comics.rb
+++ test/test_adding_comics.rb
@@ -1,12 +1,13 @@
+require 'test_helper'
 
 
 class TestAddingComicbook < Test::Unit::TestCase
   def test_takes_arguments_and_saves_them
     # start with no projects
     assert_equal Comicbook.count, 0
-    # `ruby the_comitory add superman`
-    Comicbook.create( name: 'superman' )
-    # end up with a 'superman' comic
+    # `ruby the_comitory add foo`
+    Comicbook.create( name: 'foo' )
+    # end up with a 'foo' comic
     assert_equal Comicbook.count, 1
   end
 end
