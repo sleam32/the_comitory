9fa7385e6e284c23a34f08eba0eeb57fcbdaf751
Shearry Malone <sleam32@gmail.com>
2013-05-20 15:05:20 -0500

    List feature added

--- app/controllers/comitory_controller.rb
+++ app/controllers/comitory_controller.rb
@@ -1,8 +1,56 @@
 class ComitoryController
-	attr_accessor: :params
+  attr_accessor :params
+  ACTIONS = "Actions: list, add, remove, quit"
 
-	def initialize params
-		@params = params
-	end
-	# put all methods for comitory.rb to pull from here
+  def initialize params
+    @params = params
+  end
+
+  def add_comic
+  clear_screen = "\e[H\e[2J"
+  puts clear_screen
+  puts "Add Your Comic!"
+  comic_book = $stdin.gets.chomp
+    comic = ComicBook.create(title: comic_book, publisher: comic_book, issue_number: comic_book)
+  if comic.save
+    puts "Comic Added!"
+    puts ACTIONS
+  else
+    puts "The force field has been broken. Please try re-adding this comic."
+  end
+end
+
+
+def list_comic
+  clear_screen = "\e[H\e[2J"
+  puts clear_screen
+  puts "View Comic Collection"
+  comics = ComicBook.all
+  comics.each_with_index do |comic, i|
+    unless comics.empty?
+    puts "#{i+1}. #{comic.title}"
+else
+  puts "No comics have been added!"
+  puts ACTIONS
+  end
+end
+
+
+def remove_comic
+  clear_screen = "\e[H\e[2J"
+  puts clear_screen
+  puts "Enter a comic you want to remove."
+  matching_comic = ComicBook.where(title: comic_book, publisher: comic_book, issue_number: comic_book).all
+  matching_comic.each do |comic|
+  comic.destroy
+  puts "Comic sucessfully removed!"
+  puts ACTIONS
+  end
+end
+
+
+def quit_all
+    puts "\n\n<<< Goodbye! Keep Calm and Collect On! >>>\n\n"
+  end
+end
 end
--- comitory.rb
+++ comitory.rb
@@ -1,64 +1,37 @@
 #!/usr/bin/env ruby
+require_relative 'app/controllers/comitory_controller'
 require_relative 'bootstrap_ar'
 database = ENV['COMITORY_ENV'] || 'development'
-connect_to database 
+connect_to database
+
+puts "\n\n<<< Welcome to The ComiTory >>>\n\n"
+puts "This is an interactive Comic Book collection to help organize your comic stash!\n\n"
+
+puts ComitoryController::ACTIONS
 
 
-ACTIONS = "Actions: list, add, remove, quit"
-clear_screen = "\e[H\e[2J"
-
-	puts "\n\n<<< Welcome to The ComiTory >>>\n\n"
-	puts "This is an interactive Comic Book collection to help organize your comic stash!\n\n"
-
-	puts ACTIONS
 
 command = ARGV[0]
 
-# params = {command: ARGV[0], comic:{name: ARGV[1]}}
-# controller = ComicsController.new(params)
+params = {command: ARGV[0], comic:{ name: ARGV[1] }}
+controller = ComitoryController.new(params)
 
 # router:
 if command == "add"
-	puts clear_screen
-	puts "Add Your Comic!"
-	comic_book = $stdin.gets.chomp
-	comic = ComicBook.create(title: comic_book, publisher: comic_book, issue_number: comic_book)
-	if comic.save
-		puts "Comic Added!"
-		puts ACTIONS
-	else
-		puts "The force field has been broken. Please try re-adding this comic."
-end 
+	controller.add_comic
 end
 
 if command == "list"
-	puts clear_screen
-	puts "View Comic Collection"
-	comics = ComicBook.all
-	comics.each_with_index do |comics, i|
-	# unless comics.empty?
-		puts "{i + 1}. #{ComicBook.name}"
-# else
+	controller.list_comic
 end
-	puts "No comics have been added!"
-	puts ACTIONS
-end
-# end
 
 if command == "remove"
-	puts clear_screen
-	puts "Enter a comic you want to remove."
-	matching_comic = ComicBook.where(title: comic_book, publisher: comic_book, issue_number: comic_book).all
-	matching_comic.each do |comic|
-	comic.destroy
-	puts "Comic sucessfully removed!"
-	puts ACTIONS
-end
+	conroller.remove_comic
 end
 
 if command == "quit"
-	puts "\n\n<<< Goodbye! Keep Calm and Collect On! >>>\n\n"
+	controller.quit_all
 end
-# end
+
 
 
