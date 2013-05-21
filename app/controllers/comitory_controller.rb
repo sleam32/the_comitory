d4f66b1a0f7d6e5ac41fda1ef25985b82bb05cbd
Shearry Malone <sleam32@gmail.com>
2013-05-21 11:26:44 -0500

    Fix Indentation/Remove Feature Added

--- app/controllers/comitory_controller.rb
+++ app/controllers/comitory_controller.rb
@@ -7,54 +7,56 @@ class ComitoryController
   end
 
   def add_comic
-  clear_screen = "\e[H\e[2J"
-  puts clear_screen
-  puts "Add Your Comic!"
-  comic_book = $stdin.gets.chomp
+    clear_screen = "\e[H\e[2J"
+    puts clear_screen
+    puts "Add Your Comic!"
+    comic_book = $stdin.gets.chomp
     comic = ComicBook.create(title: comic_book, publisher: comic_book, issue_number: comic_book)
-  if comic.save
-    puts "Holy Jack in the box! Comic Added!"
-    puts ACTIONS
-  else
-    puts "I'm through talking. Get out of my cave. Please try re-adding this comic."
+    if comic.save
+      puts "Holy Jack in the box! Comic Added!"
+      puts ACTIONS
+    else
+      puts "I'm through talking. Get out of my cave. Please try re-adding this comic."
+    end
   end
-end
 
 
-def list_comic
-  clear_screen = "\e[H\e[2J"
-  puts clear_screen
-  puts "View Comic Collection"
-  comics = ComicBook.all
-  comics.each_with_index do |comic, i|
-    unless comics.empty?
-    puts "#{i+1}. #{comic.title}"
-else
-  puts "Puny humans. No comics have been added!"
-  puts ACTIONS
+  def list_comic
+    clear_screen = "\e[H\e[2J"
+    puts clear_screen
+    puts "View Comic Collection"
+    comics = ComicBook.all
+    comics.each_with_index do |comic, i|
+      unless comics.empty?
+        puts "#{i+1}. #{comic.title}"
+      else
+        puts "Puny humans. No comics have been added!"
+        puts ACTIONS
+      end
+    end
   end
-end
 
 
-def remove_comic
-  clear_screen = "\e[H\e[2J"
-  puts clear_screen
-  puts "Enter a comic you want to remove."
-  comic_book = $stdin.gets.chomp
-  matching_comics = ComicBook.where(name: params[:title][:publisher][:issue_number]).all
-  matching_comics.each do |comic|
-  if comic.destroy
-    puts "Holy haberdashery, Batman!! Comic sucessfully removed!"
-  else
-  puts "Hulk smash!! Removal failed. Try again."
-  puts ACTIONS
+  def remove_comic
+    clear_screen = "\e[H\e[2J"
+    puts clear_screen
+    puts "Enter a comic you want to remove."
+    comic_book = $stdin.gets.chomp
+    matching_comics = ComicBook.where(name: params[:title][:publisher][:issue_number]).all
+    matching_comics.each do |comic|
+      if comic.destroy
+        puts "Holy haberdashery, Batman!! Comic sucessfully removed!"
+      else
+        puts "Hulk smash!! Removal failed. Try again."
+        puts ACTIONS
+      end
+    end
   end
-end
 
 
-def quit_all
+  def quit_all
+    clear_screen = "\e[H\e[2J"
+    puts clear_screen
     puts "\n\n<<< With great power comes great responsibilty. Goodbye! >>>\n\n"
   end
-end
-end
 end
