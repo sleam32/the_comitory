76e40a290a6b166910c3415d23372f13d07a77fb
Shearry Malone <sleam32@gmail.com>
2013-05-22 12:20:24 -0500

    Add Ascii Intro

--- comitory.rb
+++ comitory.rb
@@ -4,11 +4,19 @@ require_relative 'bootstrap_ar'
 database = ENV['COMITORY_ENV'] || 'development'
 connect_to database
 
+puts intro = <<EOS
+_______  __   __  _______    _______  _______  __   __  ___   _______  _______  ______    __   __
+|       ||  | |  ||       |  |       ||       ||  |_|  ||   | |       ||       ||    _ |  |  | |  |
+|_     _||  |_|  ||    ___|  |       ||   _   ||       ||   | |_     _||   _   ||   | ||  |  |_|  |
+|   |  |       ||   |___   |       ||  | |  ||       ||   |   |   |  |  | |  ||   |_||_ |       |
+|   |  |       ||    ___|  |      _||  |_|  ||       ||   |   |   |  |  |_|  ||    __  ||_     _|
+|   |  |   _   ||   |___   |     |_ |       || ||_|| ||   |   |   |  |       ||   |  | |  |   |
+|___|  |__| |__||_______|  |_______||_______||_|   |_||___|   |___|  |_______||___|  |_|  |___|
+EOS
+puts "\n\n<<< Welcome to The ComiTory, an interactive Comic Book collection to organize your comic stash! >>>\n\n"
+puts "To get started, choose an action: List, Add, Remove, Quit"
 
-puts "\n\n<<< Welcome to The ComiTory >>>\n\n"
-puts "This is an interactive Comic Book collection to help organize your comic stash!\n\n"
-
-puts ComitoryController::ACTIONS
+ComitoryController::ACTIONS
 
 
 command = ARGV[0]
@@ -32,5 +40,6 @@ end
 if command == "quit"
   controller.quit_all
 end
+
 
 
